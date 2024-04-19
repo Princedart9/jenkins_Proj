63df0dd4b29a280fcca173fa5feb86e156b1d9c8
Prince Kumar <princedart9d@gmail.com>
2024-04-20 02:48:15 +0530

    Update Jenkins_Demo.java

--- Jenkins_Demo.java
+++ Jenkins_Demo.java
@@ -3,5 +3,6 @@
     public static void main(String[] args) {
         System.out.println("Hello Jenkins are you working?: YES");
         System.out.println("Working fine!!");
+        System.out.println("Process Automation!!");
     }
  }
